7fa0c1c4ac0df5a7b445d846dbecbc30f7204dc9
Tako Schotanus <tako@ceylon-lang.org>
2017-03-02 13:52:11 +0100

    Updated for 1.3.2

--- template/source/com/redhat/demo/graph/module.ceylon
+++ template/source/com/redhat/demo/graph/module.ceylon
@@ -1,6 +1,6 @@
 "Ceyon web example - Client side"
 by ("Tako Schotanus", "Gavin King")
 native("js")
-module com.redhat.demo.graph "1.3.1" {
-    import ceylon.numeric "1.3.1";
+module com.redhat.demo.graph "1.3.2" {
+    import ceylon.numeric "1.3.2";
 } 
--- template/web-content/js/graph.js
+++ template/web-content/js/graph.js
@@ -1,5 +1,5 @@
 
-var ceylonVersion = "1.3.1";
+var ceylonVersion = "1.3.2";
 
 var langMod = 'ceylon/language/' + ceylonVersion + '/ceylon.language-' + ceylonVersion;
 
@@ -11,13 +11,14 @@ require.config({
             'ceylon/language/1.2.0/ceylon.language-1.2.0': langMod,
             'ceylon/language/1.2.1/ceylon.language-1.2.1': langMod,
             'ceylon/language/1.2.2/ceylon.language-1.2.2': langMod,
-            'ceylon/language/1.3.0/ceylon.language-1.3.0': langMod
+            'ceylon/language/1.3.0/ceylon.language-1.3.0': langMod,
+            'ceylon/language/1.3.1/ceylon.language-1.3.1': langMod
 			/* Don't put the latest version here, only previous versions */
         }
     }
 });
 
-require(['com/redhat/demo/graph/1.3.1/com.redhat.demo.graph-1.3.1'], function(graph) {
+require(['com/redhat/demo/graph/1.3.2/com.redhat.demo.graph-1.3.2'], function(graph) {
     graph.run();
 });
 
